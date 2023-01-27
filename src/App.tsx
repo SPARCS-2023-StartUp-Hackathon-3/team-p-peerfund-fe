f96942e9af25ed924fdeda7adb3b0ef23b4803a9
jiheon788 <jiheon788@ajou.ac.kr>
2023-01-28 01:21:21 +0900

    style: 전역 스타일링 - 폰트, 스타일 리셋

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React, { FunctionComponent } from "react";
-import { IntlProvider } from "react-intl";
-import CommonRouter from "./CommonRouter";
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { FunctionComponent } from 'react';
+import { IntlProvider } from 'react-intl';
+import CommonRouter from './CommonRouter';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import {
-  useRecoilValue,
-} from "recoil";
-import langState, { messages } from "@/state/lang";
-import NavBar from "./components/NavBar";
+import { useRecoilValue } from 'recoil';
+import langState, { messages } from '@/state/lang';
+import NavBar from './components/NavBar';
+import GlobalStyle from './style/GlobalStyle';
 
 const App: FunctionComponent<any> = () => {
   const lang = useRecoilValue(langState);
 
   return (
     <IntlProvider locale={lang} messages={messages[lang]}>
+      <GlobalStyle />
       <Router>
         <div style={{ display: 'flex', flexDirection: 'column', alignContent: 'conter', height: '100%' }}>
           <NavBar />
