f9a2884c5fcd4ea19e83b315a8766e6420e05b4c
Dongho Gang <gongdongho12@gmail.com>
2023-01-27 23:51:32 +0900

    refactor: 전체적인 개발툴 환경 설정 eslint + prettier + vscode

--- src/components/CommonForm/CommonForm.tsx
+++ src/components/CommonForm/CommonForm.tsx
@@ -1,12 +1,67 @@
+import { Button, Cascader, Form, Input, InputNumber, Radio, Select, Switch, TreeSelect } from 'antd';
 import React, { FunctionComponent } from 'react';
 
-interface ICommonFormProps {
-}
+interface ICommonFormProps {}
+
+const { useForm } = Form;
+
+const formCols = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 14 },
+};
 
 const CommonForm: FunctionComponent<ICommonFormProps> = (props) => {
-  return <div>
-    
-  </div>;
+  const [form] = useForm();
+  return (
+    <Form form={form}>
+      <Form.Item label="Form Size" name="size">
+        <Radio.Group>
+          <Radio.Button value="small">Small</Radio.Button>
+          <Radio.Button value="default">Default</Radio.Button>
+          <Radio.Button value="large">Large</Radio.Button>
+        </Radio.Group>
+      </Form.Item>
+      <Form.Item label="Input">
+        <Input />
+      </Form.Item>
+      <Form.Item label="Select">
+        <Select>
+          <Select.Option value="demo">Demo</Select.Option>
+        </Select>
+      </Form.Item>
+      <Form.Item label="TreeSelect">
+        <TreeSelect
+          treeData={[
+            {
+              title: 'Light',
+              value: 'light',
+              children: [{ title: 'Bamboo', value: 'bamboo' }],
+            },
+          ]}
+        />
+      </Form.Item>
+      <Form.Item label="Cascader">
+        <Cascader
+          options={[
+            {
+              value: 'zhejiang',
+              label: 'Zhejiang',
+              children: [{ value: 'hangzhou', label: 'Hangzhou' }],
+            },
+          ]}
+        />
+      </Form.Item>
+      <Form.Item label="InputNumber">
+        <InputNumber />
+      </Form.Item>
+      <Form.Item label="Switch" valuePropName="checked">
+        <Switch />
+      </Form.Item>
+      <Form.Item label="Button">
+        <Button>Button</Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default CommonForm;
