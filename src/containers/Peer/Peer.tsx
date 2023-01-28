5cf5f76bb6289f571860cdd53ae13506358d8046
jiheon788 <jiheon788@ajou.ac.kr>
2023-01-28 19:11:34 +0900

    feat: 무한스크롤 및 디자인 변경에 따른 수정

--- src/containers/Peer/Peer.tsx
+++ src/containers/Peer/Peer.tsx
@@ -1,8 +1,38 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, CSSProperties } from 'react';
 import DefaultLayout from '@/components/DefaultLayout';
+import Banner from '@/components/Banner';
+import ProjectList from '@/components/ProjectList';
+import styled from 'styled-components';
 
-const Peer = (props) => {
-  return <DefaultLayout>Peer</DefaultLayout>;
+interface ICardViewProps {}
+
+interface Props {
+  style?: CSSProperties;
+}
+
+const Container = styled.div`
+  width: 85%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+
+  @media screen and (max-width: 700px) {
+    width: 100%;
+  }
+`;
+
+const Peer: FunctionComponent<ICardViewProps> = (props) => {
+  const jobTypes = ['기획자', '디자이너', '프론트엔드', '백엔드', '기타'];
+  const projectTypes = ['전체', '프로젝트', '스터디'];
+  const fundingStatus = ['전체', '펀딩 진행중', '공개예정'];
+
+  return (
+    <Container>
+      <Banner />
+      <ProjectList title={'주목 할 만한 모집 공고'} firstCategories={jobTypes} secondCategories={projectTypes} />
+    </Container>
+  );
 };
 
 export default Peer;
