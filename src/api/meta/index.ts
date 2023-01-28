5330ee9f08f2a6867423b92da0c38e62da5adf37
Dongho Gang <gongdongho12@gmail.com>
2023-01-28 19:13:06 +0900

    refactor: 타임아웃 증가

--- src/api/meta/index.ts
+++ src/api/meta/index.ts
@@ -1,6 +1,6 @@
 const prodConfig = {
   baseURL: 'http://ec2-54-180-159-18.ap-northeast-2.compute.amazonaws.com:8080',
-  timeout: 2500,
+  timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 };
 
